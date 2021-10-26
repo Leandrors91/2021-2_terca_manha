1d1abbb93758540816731332a9160cf3017d3578
Prof. Thiago G. Traue <t.traue@gmail.com>
2021-10-26 11:42:34 -0300

    Continuação do projeto

--- uni_clima/lib/main.dart
+++ uni_clima/lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:uni_clima/screens/home.dart';
+import 'package:uni_clima/theme/theme.dart';
 
 void main() {
-  
+  runApp(const UniClima());
+}
+
+class UniClima extends StatelessWidget {
+  const UniClima({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+      title: "UniClima",
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      themeMode: ThemeMode.system,
+    );
+  }
 }
